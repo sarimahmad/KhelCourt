c67639628e9e7c769638a20c714ee6e399657775
SaeedAhmed <saeedahmed2023@gmail.com>
2022-08-01 11:47:29 +0500

    ios push notification

--- ios/KhelCourt/AppDelegate.mm
+++ ios/KhelCourt/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -30,6 +31,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   RCTAppSetupPrepareApp(application);
+  [FIRApp configure];
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
 
